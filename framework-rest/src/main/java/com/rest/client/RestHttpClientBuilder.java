13fb223d0ca8729a8e6fc2022d26a4b516ec57d7
halbert918 <halbert918@163.com>
2018-04-19 16:43:56 +0800

    添加对httpclient连接池空闲连接回收处理

--- framework-rest/src/main/java/com/rest/client/RestHttpClientBuilder.java
+++ framework-rest/src/main/java/com/rest/client/RestHttpClientBuilder.java
@@ -10,12 +10,15 @@ import org.apache.http.conn.ConnectionKeepAliveStrategy;
 import org.apache.http.conn.HttpClientConnectionManager;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
+import org.apache.http.impl.client.DefaultHttpRequestRetryHandler;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.message.BasicHeaderElementIterator;
 import org.apache.http.protocol.HTTP;
 import org.apache.http.protocol.HttpContext;
 import org.apache.http.util.Args;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Vesrion 1.0
@@ -31,13 +34,17 @@ public class RestHttpClientBuilder extends HttpClientBuilder {
 
     public CloseableHttpClient build() {
         super.setConnectionManager(buildConnectionManager())
-                .setKeepAliveStrategy(new CustomConnectionKeepAliveStrategy());
+                .setKeepAliveStrategy(new CustomConnectionKeepAliveStrategy())
+                //重试次数
+//                .setRetryHandler(new DefaultHttpRequestRetryHandler(0, false))
+                ;
         return super.build();
     }
 
     protected HttpClientConnectionManager buildConnectionManager() {
         PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
         configConnectionManager(connectionManager);
+        startMonitorThread(connectionManager);
         return connectionManager;
     }
 
@@ -50,14 +57,25 @@ public class RestHttpClientBuilder extends HttpClientBuilder {
         }
     }
 
+    protected void startMonitorThread(HttpClientConnectionManager connectionManager) {
+        IdleConnectionMonitorThread monitorThread = new IdleConnectionMonitorThread(connectionManager);
+        monitorThread.start();
+    }
+
+    /**
+     * Interface for deciding how long a connection can remain
+     * idle before being reused.
+     * 自定义连接存活策略，即当前连接最大的空闲时长，
+     * 超过该时长，则该连接必须可以被其他请求重新使用
+     */
     @Immutable
     private class CustomConnectionKeepAliveStrategy implements ConnectionKeepAliveStrategy {
-        public final DefaultConnectionKeepAliveStrategy INSTANCE = new DefaultConnectionKeepAliveStrategy();
-
-        private final int KEEP_ALIVE_TIME_OUT = 60000;
+        // 连接空闲时间
+        private final int KEEP_ALIVE_TIME_OUT = 6000;
 
         public long getKeepAliveDuration(final HttpResponse response, final HttpContext context) {
             Args.notNull(response, "HTTP response");
+            //获取服务端最后响应数据
             final HeaderElementIterator it = new BasicHeaderElementIterator(
                     response.headerIterator(HTTP.CONN_KEEP_ALIVE));
             while (it.hasNext()) {
@@ -74,4 +92,47 @@ public class RestHttpClientBuilder extends HttpClientBuilder {
             return KEEP_ALIVE_TIME_OUT;
         }
     }
+
+    /**
+     * 监控关闭过期的连接
+     * 监控关闭超过指定空闲时间的连接
+     */
+    public static class IdleConnectionMonitorThread extends Thread {
+
+        private HttpClientConnectionManager connMgr;
+
+        private volatile boolean shutdown;
+
+        public IdleConnectionMonitorThread(HttpClientConnectionManager connMgr) {
+            super();
+            this.connMgr = connMgr;
+        }
+
+        @Override
+        public void run() {
+            try {
+                while (!shutdown) {
+                    synchronized (this) {
+                        wait(5000);
+                        // Close expired connections
+                        connMgr.closeExpiredConnections();
+                        // Optionally, close connections
+                        // that have been idle longer than 30 sec
+                        connMgr.closeIdleConnections(30, TimeUnit.SECONDS);
+                    }
+                }
+            } catch (InterruptedException e) {
+                // terminate
+            }
+        }
+
+        public void shutdown() {
+            shutdown = true;
+            synchronized (this) {
+                notifyAll();
+            }
+        }
+
+    }
+
 }
